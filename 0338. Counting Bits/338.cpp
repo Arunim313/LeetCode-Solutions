edd17195a3e6b746a95eb709e21500b810f6a121
Arunim Malviya <85449742+Arunim313@users.noreply.github.com>
2022-08-11 10:16:31 +0530

    Add files via upload

---
+++ 0338. Counting Bits/338.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int counter(int x )
+    {   
+        int count = 0;
+        if(x==1 || x==2) return 1;
+        while(x!=0)
+        { 
+         if(x&1)
+         {
+             count++;
+         }
+            x=(x>>1);
+         }
+        return count ;
+        
+        
+     }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i =0;i<n+1;i++){
+        cout<<"i = "<<i<<"  "<<endl;
+            int count = counter(i);
+            ans.push_back(count);
+        }
+        return ans;}
+};llrrrrr
