a6ce9de09b8f8dbe6b2e1b7ad20844771b3e8d01
Arunim Malviya <85449742+Arunim313@users.noreply.github.com>
2022-08-17 19:58:43 +0530

    Add files via upload

---
+++ 0054. Spiral Matrix/54.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int count = 0;
+        int total = row*col;
+        
+        // index initialisation
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+        
+        while(count < total){
+            
+            //printing starting row
+            for(int index=startingCol; count<total && index<=endingCol; index++){
+                ans.push_back(matrix[startingRow][index]); 
+                count++;
+            }
+            startingRow++;
+            
+            //printing ending col
+            for(int index=startingRow; count<total && index<=endingRow; index++){
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+            
+            //printing ending row
+            for(int index=endingCol; count<total && index>=startingCol; index--){
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+            
+            //printing starting col;
+            for(int index=endingRow; count<total && index>=startingRow; index--){
+                ans.push_back(matrix[index][startingCol]);  
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};
